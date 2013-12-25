42cbd1b704add35a169e009da0c6b2cb65a622a1
nyarla <nyarla@thotep.net>
2013-12-25 14:02:51 +0900

    Cask: added Teitoku.app

---
+++ Casks/teitoku.rb
@@ -0,0 +1,7 @@
+class Teitoku < Cask
+  url 'http://geta6.net/teitoku/teitoku-0.2.9-osx.tar.gz'
+  homepage 'http://makebooth.com/i/1xkN1'
+  version '0.2.9'
+  sha1 '6ee08f1e565dce0609c740ee272f6b6acaf2f1ab'
+  link 'Teitoku.app'
+end
