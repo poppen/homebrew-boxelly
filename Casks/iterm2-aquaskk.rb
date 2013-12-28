2c43b4ecfce4b6151543aaffc25d5881358e774d
poppen <poppen.jp@gmail.com>
2013-12-28 12:41:35 +0900

    Cask: added iTerm2.app with AquaSKK support

---
+++ Casks/iterm2-aquaskk.rb
@@ -0,0 +1,7 @@
+class Iterm2Aquaskk < Cask
+  url 'https://github.com/downloads/mrkn/iterm2/iTerm-1.0-aquaskk.app.zip'
+  homepage 'https://github.com/mrkn/iterm2'
+  version '1.0'
+  sha1 '5ffec1b8e820a49730660e95670ecdfaab3e7e0b'
+  link 'iTerm.app'
+end
