0b9100083ca895bf518c4e093cddd56a57e5dbb5
nyarla <nyarla@thotep.net>
2014-01-07 16:47:32 +0900

    Casks: added Coccinellida.app

---
+++ Casks/coccinellida.rb
@@ -0,0 +1,8 @@
+class Coccinellida < Cask
+  url 'http://sourceforge.net/projects/coccinellida/files/latest/download'
+  homepage 'http://coccinellida.sourceforge.net/'
+  version '0.6.1'
+  sha1 '3ed0380f516e7c7f1dd51fc7f2371c5b6edf0800'
+  link 'Coccinellida.app'
+end
+
