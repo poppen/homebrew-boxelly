1f9495476eac2f12728407eb7689661ea882bb54
nyarla <nyarla@thotep.net>
2014-08-28 13:54:18 +0900

    macvim-kaoriya: supports latest brew-cask

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -1,7 +1,9 @@
 class MacvimKaoriya < Cask
+  version '20140805'
+  sha256 'a47e42e32ddb3dd26e65e1ccfac532474da2a89fb516eb3a480b4f0ddabe4235'
+
   url 'https://github.com/splhack/macvim/releases/download/20140805/macvim-kaoriya-20140805.dmg'
   homepage 'https://code.google.com/p/macvim-kaoriya/'
-  version '20140805'
-  sha1 '1587551a823f9dd85bf5ea09c58b8bcd0b29a1cb'
+  
   link 'MacVim.app'
 end
