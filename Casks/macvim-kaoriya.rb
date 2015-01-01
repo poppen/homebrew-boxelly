4753b3e040e87204397232d53dfe1d4424069177
nyarla <nyarla@thotep.net>
2015-01-01 21:35:24 +0900

    macvim-kaoriya: update cask format

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -1,9 +1,10 @@
-class MacvimKaoriya < Cask
+cask :v1 => 'macvim-kaoriya' do
   version '20141204'
   sha256 '38f6e67d5fd8edc25828ec297a0b73c5865873dd63d5325f6b2936184e5c664f'
 
   url 'https://github.com/splhack/macvim/releases/download/20141204/macvim-kaoriya-20141204.dmg'
   homepage 'https://code.google.com/p/macvim-kaoriya/'
-  
+  license :oss
+
   link 'MacVim.app'
 end
