4c18affb1b26261661aaf14e8392899d1fa3a44d
poppen <poppen.jp@gmail.com>
2015-01-03 00:15:46 +0900

    iterm2-aquaskk: update cask format

--- Casks/iterm2-aquaskk.rb
+++ Casks/iterm2-aquaskk.rb
@@ -1,7 +1,13 @@
-class Iterm2Aquaskk < Cask
+cask :v1 => 'iterm2-aquaskk' do
+  version '1.0'
+  sha256 'b37898f7568da019ee756bce68eb0839436d59bcea519b03f329734b8b02c888'
+
   url 'https://github.com/downloads/mrkn/iterm2/iTerm-1.0-aquaskk.app.zip'
   homepage 'https://github.com/mrkn/iterm2'
-  version '1.0'
-  sha1 '5ffec1b8e820a49730660e95670ecdfaab3e7e0b'
-  link 'iTerm.app'
+
+  license :gpl
+
+  app 'iTerm.app'
+
+  zap :delete => '~/Library/Preferences/com.googlecode.iterm2.plist'
 end
